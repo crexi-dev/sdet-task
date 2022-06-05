56792400bc55eddccbcdb49d3b08260092b4f373
nandinakartheek <nandika@repsrv.com>
2022-06-05 14:07:34 -0700

    added testcases for crud - adding, editing and deleting book data

--- projects/angular-ngrx-material-starter/cypress/integration/crud-books.spec.ts
+++ projects/angular-ngrx-material-starter/cypress/integration/crud-books.spec.ts
@@ -7,28 +7,40 @@ describe('CRUD Books Test', () => {
 
     it('adds a book', () => {
       cy.visit('/examples/crud/');
+
+      cy.log('Adding a new Book');
+
       cy.get('button.add .ng-fa-icon').click();
       cy.get('#mat-input-0').type(title);
       cy.get('#mat-input-1').type(author);
       cy.get('#mat-input-2').type(description);
       cy.contains('Save').click();
+      cy.contains(title).should('be.visible');
     });
 
 
     it('edits a book', () => {
       cy.contains(title).click();
+
+      cy.log('Editing the Book');
+
       cy.get('fa-icon[icon="edit"]').click()
       cy.get('#mat-input-0').clear();
       cy.get('#mat-input-0').type(updatedTitle);
       cy.contains('Save').click();
+      cy.log('Verify book updated title should exist');
       cy.contains(updatedTitle).should('be.visible');
+      cy.log('Verify book initial title should exist');
       cy.contains(title).should('not.exist');
     });
 
 
     it('deletes a book', () => {
       cy.contains(updatedTitle).click();
-      cy.get('fa-icon[icon="trash"]').click()
+      cy.log('Deleting the Book');
+
+      cy.get('fa-icon[icon="trash"]').click();
+      cy.log('Verify book title should not exist');
       cy.contains(updatedTitle).should('not.exist');
     });
 
