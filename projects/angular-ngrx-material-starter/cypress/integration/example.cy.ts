b38d075e51003feb536c017b7646d58b1b74925f
Robert Tassy <robtassy@gmail.com>
2022-12-10 13:59:14 -0800

    refactor

--- projects/angular-ngrx-material-starter/cypress/integration/example.cy.ts
+++ projects/angular-ngrx-material-starter/cypress/integration/example.cy.ts
@@ -1,5 +1,17 @@
 import { faker } from '@faker-js/faker';
+const titleText = faker.lorem.sentence(3);
+const authorText = faker.lorem.sentence(2);
+const descText = faker.lorem.sentence(5);
+
 const booksHeader = '.container > :nth-child(2) > :nth-child(1) > h2';
+const nullBooksHeader = 'span.justify-content-between > h2';
+const titleField = '#mat-input-0';
+const authorField = '#mat-input-1';
+const descField = '#mat-input-2';
+const saveButton = '.col-12 > .mat-primary';
+const editButton = 'span.ng-star-inserted > .mat-accent';
+const deleteButton = '.mat-warn';
+const readField = '.container';
 
 
 describe('CRUD Flow', () => {
@@ -7,51 +19,60 @@ describe('CRUD Flow', () => {
     cy.visit('/');
     cy.contains('Crexi SDET Task');
     cy.get('.mat-button').contains('Examples');
+
   });
 
   it('Navigate CRUD tab', () => { 
     cy.visit('/examples/crud/');
-    cy.get(booksHeader).contains('Your books') //unless static, consider adding data-cy tag for specific h2 tag
+    cy.get(booksHeader).contains('Your books')
+
   });
 
-  it('Creation flow and read check', () => {
-    const titleText = faker.lorem.sentence(4);
-    const authorText = faker.lorem.sentence(2);
-    const descText = faker.lorem.sentence(5);
+  it('Create a new book', () => {
     cy.visit('/examples/crud/');
     cy.get('.add').click()
-    cy.get('#mat-input-0').type(titleText)
-    cy.get('#mat-input-1').type(authorText)
-    cy.get('#mat-input-2').type(descText)
-    cy.get('.col-12 > .mat-primary').click()
-    cy.get('.container').contains(titleText + authorText)
+    cy.get(titleField).type(titleText)
+    cy.get(authorField).type(authorText)
+    cy.get(descField).type(descText)
+    cy.get(saveButton).click()
+    cy.get(readField).contains(titleText + authorText) // Verifies successful Create and Read
+
   });     
   
-  it('Update flow and error msg check', () => {
-    const textGeneration = faker.lorem.sentence(5);
-    cy.visit('/examples/crud/123');
-    cy.get('span.ng-star-inserted > .mat-accent').click()
-    cy.get('#mat-input-0').click().clear()
-    cy.get('.col-12 > .mat-primary').click() //save
-    cy.get('#mat-error-2').should('contain', 'Title is required')
-    cy.get('#mat-input-0').click().type('Reactive Programming with Angular and ngrx')
-    cy.get('.col-12 > .mat-primary').click()
-    cy.get('span.ng-star-inserted > .mat-accent').click()
-    cy.get('#mat-input-2').click().clear().type(textGeneration)
-    cy.get('.col-12 > .mat-primary').click()
-    cy.get('p').contains(textGeneration)
-  }); 
+  it('Update an existing book: Error state', () => {
+    const titleError = '#mat-error-2'
+    const authorError = '#mat-error-3'
 
-  it('Deletion flow and check deleted id routes to home screen', () => {
     cy.visit('/examples/crud/123');
-    cy.get('.mat-warn').click()
-    cy.get('.content').should('not.contain', 'React Programming')
-    cy.visit('/examples/crud/123');
-    cy.get('span.justify-content-between > h2').contains('Book editor')
+    cy.get(editButton).click()
+    cy.get(titleField).click().clear()
+    cy.get(authorField).click().clear()
+    cy.get(descField).click().clear()
+    cy.get(saveButton).click()
+    cy.get(titleError).should('contain', 'Title is required') // Edge case
+    cy.get(authorError).should('contain', 'Author is required') // Edge case
 
   }); 
+  
+  it('Update an existing book', () => {
+    cy.visit('/examples/crud/123');
+    cy.get(editButton).click()
+    cy.get(titleField).click().clear().type(titleText)
+    cy.get(authorField).click().clear().type(authorText)
+    cy.get(descField).click().clear().type(descText)
+    cy.get(saveButton).click()
+    cy.get(readField).contains(titleText + authorText) // Verifies successful Update and Read
 
+  }); 
 
+  it('Delete a book', () => {
+    cy.visit('/examples/crud/123');
+    cy.get(deleteButton).click()
+    cy.get(readField).should('not.contain', 'React Programming') // Verifies successful Delete
+    cy.visit('/examples/crud/123'); // Edge case: Navigated to deleted book
+    cy.get(nullBooksHeader).contains('Book editor') // Edge case: Verifies user is directed user to home view
+
+  }); 
 
 
 });
